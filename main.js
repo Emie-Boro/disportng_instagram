bf9badf3d1389cc2b47b17454e291e85bb1b021e
Emie Boro <emiedonmukumo@gmail.com>
2024-01-07 15:12:49 +0100

    Updated files

--- main.js
+++ main.js
@@ -105,14 +105,81 @@ app.post('/signup', async (req,res)=>{
     res.redirect('/login')
 })
 
-app.get('/dashboard', (req, res)=>{
-    if(req.isAuthenticated()) {
-        res.render('dashboard', {
-            layout:'dashboard'
-        })
-    } else{
-        res.redirect('/login')
+app.get('/dashboard', ensureAuthenticated, (req, res)=>{
+    res.render('dashboard', {
+        layout:'dashboard'
+    })
+})
+
+app.get('/newInstagramPost', ensureAuthenticated, (req, res)=>{
+    res.render('newInstagramPost', {
+        layout: 'dashboard'
+    })
+})
+app.post('/export', ensureAuthenticated, upload.single('image'), (req,res)=>{
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
     }
+    const imageBuffer = req.file.buffer;
+    const base64Image = imageBuffer.toString('base64');
+    const imageURL = `data:${req.file.mimetype};base64,${base64Image}`;
+
+    res.render('export', {
+        layout: 'dashboard',
+        imageURL,
+        title: req.body.title,
+        content:req.body.content
+    })
+})
+
+// ___________________________Story Post ___________________________________________
+app.get('/newStory', (req,res)=>{
+    res.render('newStory',{
+        layout: req.isAuthenticated() ? 'dashboard' : 'main',
+    })
+})
+app.get('/stories', ensureAuthenticated, async (req,res)=>{
+    const stories = await Story.find({}).lean()
+
+    res.render('stories', {
+        layout:'dashboard',
+        title:'Stories',
+        stories:stories
+    })
+})
+
+app.post('/story/share', async (req,res)=>{
+    try {
+        // Save the story to the database
+        await new Story({
+            state: req.body.state,
+            phone: req.body.phone,
+            email: req.body.email,
+            name: req.body.name,
+            title: req.body.title,
+            content: req.body.content
+        }).save();
+
+        // Send a success response
+        res.status(200).json({ success: true, message: 'Story saved successfully' });
+    } catch (error) {
+        // Send an error response
+        res.status(500).json({ success: false, message: 'Failed to save the story' });
+    }
+})
+
+app.get('/stories/:id', ensureAuthenticated, async (req,res)=>{
+    const story = await Story.findById(req.params.id).lean()
+    res.render('story',{
+        layout:'dashboard',
+        story
+    })
+})
+
+
+app.delete('/story/:id', ensureAuthenticated, async (req,res)=>{
+    await Story.deleteOne({_id:req.params.id})
+    res.redirect('/stories')
 })
 
 app.get('/logout', (req, res)=>{
@@ -131,4 +198,6 @@ connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("listening for requests");
     })
+}).catch(err=>{
+    console.log(err)
 })
